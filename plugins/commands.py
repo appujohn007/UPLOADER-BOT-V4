9ea3d295eb8557de81cc05aa8866e3094d9cc941
Appu John <161853537+appujohn007@users.noreply.github.com>
2024-07-23 11:15:54 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -17,6 +17,9 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from plugins.database.add import add_user_to_database
 from plugins.functions.forcesub import handle_force_subscribe
+from utils import verify_user, check_token
+from database.adduser import AddUser
+
 
 @Client.on_message(filters.command(["start"]) & filters.private)
 async def start(bot, update):
@@ -38,5 +41,38 @@ async def start(bot, update):
             disable_web_page_preview=True,
             reply_markup=Translation.START_BUTTONS
         )
+        return
+
+    data = update.command[1]
+
+    if data.split("-", 1)[0] == "verify":
+        userid = data.split("-", 2)[1]
+        token = data.split("-", 3)[2]
+        if str(update.from_user.id) != str(userid):
+            return await update.reply_text(
+                text="<b>ᴇxᴘɪʀᴇᴅ ʟɪɴᴋ ᴏʀ ɪɴᴠᴀʟɪᴅ ʟɪɴᴋ !</b>",
+                protect_content=True
+            )
+        is_valid = await check_token(bot, userid, token)
+        if is_valid == True:
+            await update.reply_text(
+                text=f"<b>ʜᴇʟʟᴏ {update.from_user.mention} 👋,\nʏᴏᴜ ᴀʀᴇ sᴜᴄᴄᴇssғᴜʟʟʏ ᴠᴇʀɪғɪᴇᴅ !\n\nɴᴏᴡ ʏᴏᴜ ʜᴀᴠᴇ ᴜɴʟɪᴍɪᴛᴇᴅ ᴀᴄᴄᴇss ғᴏʀ ᴀʟʟ ᴜʀʟ ᴜᴘʟᴏᴀᴅɪɴɢ ᴛɪʟʟ ᴛᴏᴅᴀʏ ᴍɪᴅɴɪɢʜᴛ.</b>",
+                protect_content=True
+            )
+            await verify_user(bot, userid, token)
+            sender = update.from_user
+            username = f"@{sender.username}" if sender.username else f"{sender.first_name} {sender.last_name or ''}"
+            # Send message to the bot admin or log channel about the verification
+            chat_id = -1002239847745  # Replace with actual admin ID
+            thread_id = 4
+            admin_message = f"**User {username}**\nverified access with \n**URL: {data}**"
+            await bot.send_message(chat_id, admin_message, reply_to_message_id=thread_id)
+
+    
+        else:
+            return await update.reply_text(
+                text="<b>ᴇxᴘɪʀᴇᴅ ʟɪɴᴋ ᴏʀ ɪɴᴠᴀʟɪᴅ ʟɪɴᴋ !</b>",
+                protect_content=True
+            )
 
 
