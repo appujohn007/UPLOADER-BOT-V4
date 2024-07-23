b52a5cc9e0b1f37083b19bbb27afdf35fa5379a8
Appu John <161853537+appujohn007@users.noreply.github.com>
2024-07-23 11:25:02 +0530

    Update adduser.py

--- database/adduser.py
+++ database/adduser.py
@@ -1,6 +1,4 @@
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+
 
 from pyrogram import Client
 from database.access import techvj
@@ -15,4 +13,4 @@ Nᴀᴍᴇ - {}"""
 async def AddUser(bot: Client, update: Message):
     if not await techvj.is_user_exist(update.from_user.id):
            await techvj.add_user(update.from_user.id)
-           await bot.send_message(Config.TECH_VJ_LOG_CHANNEL, LOG_TEXT_P.format(update.from_user.id, update.from_user.mention))
+        #   await bot.send_message(Config.TECH_VJ_LOG_CHANNEL, LOG_TEXT_P.format(update.from_user.id, update.from_user.mention))
