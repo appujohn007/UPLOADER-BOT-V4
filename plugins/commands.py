b2ae3710939ed166f277e1af5bf5530ff5872aa2
Appu John <161853537+appujohn007@users.noreply.github.com>
2024-07-23 11:09:28 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -32,10 +32,11 @@ async def start(bot, update):
       fsub = await handle_force_subscribe(bot, update)
       if fsub == 400:
         return
-    await update.reply_text(
-        text=Translation.START_TEXT.format(update.from_user.mention),
-        disable_web_page_preview=True,
-        reply_markup=Translation.START_BUTTONS
-    )
+    if len(update.command) != 2:
+        await update.reply_text(
+            text=Translation.START_TEXT.format(update.from_user.mention),
+            disable_web_page_preview=True,
+            reply_markup=Translation.START_BUTTONS
+        )
 
 
